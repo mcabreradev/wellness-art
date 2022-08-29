4844a5379ec261cb8b192eb9b27c149b913a7f90
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2022-08-29 12:08:58 -0300

    wip

--- src/sections/Location.tsx
+++ src/sections/Location.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import tw from "tailwind-styled-components";
+import Map from '../components/Map'
+
+const Container = tw.div`
+  bg-white
+  flex
+  items-center 
+  justify-center 
+  flex-col
+`;
+
+const Title = tw.h1`
+  font-normal 
+  text-2xl
+  leading-[3.5rem] 
+  not-italic 
+  text-black
+  font-domine
+`;
+
+const Paragraph = tw.p`
+  font-normal 
+  text-lg 
+  leading-[1.8rem] 
+  not-italic 
+  text-black
+  font-poppins
+`;
 
 export default function Location() {
   return (
-    <div className={'location'}>Location</div>
+    <Container className={'location'}>
+      <div className="my-28">
+        <Title>Located in the center of Edmont</Title>
+        <Paragraph>We offer you the best instalaciones para birndarte el mejor servicio</Paragraph>
+      </div>
+
+      <Map/>
+    </Container>
   )
 }
