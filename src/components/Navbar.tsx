0589e5a1e290c37d7e8698da07b47b10a1672e75
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2022-09-05 22:08:39 -0300

    wip about us

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
   });
 
   const handleScroll = () => {
-    window.scrollY > 300 ? setSticky(true) : setSticky(false);
+    window.scrollY > 50 ? setSticky(true) : setSticky(false);
   };
 
   return (
